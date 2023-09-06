8e5936cc2c8fd9dc8015e3109c661a39eba84bda
mcbigboy <114679787+kenziebigboy@users.noreply.github.com>
2023-09-06 15:37:36 -0600

    Changed Name of Class

--- Application/src/main/java/com/kenzie/appserver/repositories/model/VehicleRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/VehicleRecord.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "Vehicle")
-public class Vehicle {
+public class VehicleRecord {
 
     String id;
     String name;
