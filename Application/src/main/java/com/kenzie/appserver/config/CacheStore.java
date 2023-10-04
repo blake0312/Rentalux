d7b6db923f5eebc3fb61d85b39a64e1d6bf14309
mcbigboy <114679787+kenziebigboy@users.noreply.github.com>
2023-10-04 09:03:27 -0600

    Added cache for ReservationData

--- Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
+++ Application/src/main/java/com/kenzie/appserver/config/CacheStore.java
@@ -1,2 +1,36 @@
-package com.kenzie.appserver.config;public class CacheStore {
+package com.kenzie.appserver.config;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.kenzie.capstone.service.model.ReservationData;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class CacheStore {
+
+    private Cache<String, List<ReservationData>> cache;
+
+    public CacheStore(int expiry, TimeUnit timeUnit){
+        this.cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expiry, timeUnit)
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .build();
+    }
+
+    public List<ReservationData> get (String key){
+        return cache.getIfPresent(key);
+    }
+
+    public void evict(String key){
+        if(key != null){
+            cache.invalidate(key);
+        }
+    }
+
+    public void add(String key, List<ReservationData> value){
+        if(key != null && value != null){
+            cache.put(key, value);
+        }
+    }
 }
