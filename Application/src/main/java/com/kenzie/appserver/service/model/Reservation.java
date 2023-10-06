8822bada6e120749a735fca2b6841d52b4c03d08
blake0312 <blake.maltos@mykenzie.snhu.edu>
2023-10-05 19:57:32 -0500

    New intermediate class

---
+++ Application/src/main/java/com/kenzie/appserver/service/model/Reservation.java
@@ -0,0 +1,58 @@
+package com.kenzie.appserver.service.model;
+
+public class Reservation {
+    private String id;
+    private String customerId;
+    private boolean payed;
+    private String vehicleId;
+    private String startData;
+    private String endData;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public boolean isPayed() {
+        return payed;
+    }
+
+    public void setPayed(boolean payed) {
+        this.payed = payed;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getStartData() {
+        return startData;
+    }
+
+    public void setStartData(String startData) {
+        this.startData = startData;
+    }
+
+    public String getEndData() {
+        return endData;
+    }
+
+    public void setEndData(String endData) {
+        this.endData = endData;
+    }
+}
